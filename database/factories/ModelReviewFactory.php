6744426f1b6e653545b605ca8c31ab5d1688ba50
saulegv <saulegv@gmail.com>
2019-03-13 15:58:11 -0600

    Seeding part for Product and Review

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -8,8 +8,8 @@ $factory->define(App\Model\Review::class, function (Faker $faker) {
         'product_id' => function(){
             return Product::all()->random();
         },
-        'customer' => $faker->name,
-        'review' => $faker->paragraph,
-        'star' => $faker-> numberBetween(0, 5)
+        'customer' => $faker->name, 
+        'review' => $faker->paragraph, 
+        'star' => $faker-> numberBetween(0, 5) 
     ];
 });
