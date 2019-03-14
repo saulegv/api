62d1d6466f113e0930ad15889b890a28bcd5ff8e
saulegv <saulegv@gmail.com>
2019-03-13 22:48:53 -0600

    Modified Product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,9 +18,14 @@ class ProductResource extends JsonResource
             'Name' => $this->name,
             'Description' => $this->detail,
             'Price' => $this->price,
-            'Stock' => $this->stock,
+            'Stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'Discount' => $this->discount,
-            
+            'TotalPrice' => round((1- ($this->discount/100)) * $this->price,2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(),2) : 'No rating yet',
+            'href' => [
+
+                'reviews' => route('reviews.index', $this->id)
+            ]
         ];
 
     }
